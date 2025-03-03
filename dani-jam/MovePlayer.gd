cad9c67e8a76e7f593d694d563eb68e9b6f1994f
liaaliz <lializ@proton.me>
2025-03-03 07:55:52 -0300

    FIX add player changes

--- dani-jam/MovePlayer.gd
+++ dani-jam/MovePlayer.gd
@@ -1,25 +1,34 @@
 extends CharacterBody2D
 
+@export var brush_pivot : Node2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED       : float = 300.0
+var direction     : Vector2 
 
+func _unhandled_input(e: InputEvent) -> void:
+  if not e is InputEventMouseMotion: return
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+  if sign(brush_pivot.rotation) > 0:
+    brush_pivot.z_index = 1
+    return
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+  brush_pivot.z_index = -1
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+func _unhandled_key_input(e : InputEvent) -> void:
+  if not e.is_action_type(): return
+  ##if player is on stagger : return
+  direction = Vector2(
+    Input.get_axis("ui_left","ui_right"),
+    Input.get_axis("ui_up",  "ui_down")
+  )
 
-	move_and_slide()
+func _process(delta: float) -> void: 
+  var new_brush_angle = brush_pivot.position.angle_to_point(get_global_mouse_position())
+  brush_pivot.global_rotation = lerp_angle(brush_pivot.global_rotation, new_brush_angle, delta * 7)
+
+func _physics_process(_delta: float) -> void:
+    move_and_slide()
+    if direction: 
+      velocity = direction.normalized() * SPEED 
+      return
+    velocity = Vector2.ZERO
